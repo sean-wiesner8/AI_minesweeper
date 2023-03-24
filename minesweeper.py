d4e17ea86ff96ee75660442f24deef62e0d9faf6
Frank Zhou <fzcuber@gmail.com>
2023-03-24 10:35:22 -0400

    handle invalid input

--- minesweeper.py
+++ minesweeper.py
@@ -13,8 +13,7 @@ unopened = -1
 flaged = -2
 
 # the change in tile for each of the 8 surrounding tiles
-coordinates = {(-1, -1), (-1, 0), (-1, 1), (0, -1),
-               (0, 1), (1, -1), (1,  0), (1, 1)}
+coordinates = {(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)}
 
 # initalize minesweeper with a [size] x [size] board and mine count [mine]
 
@@ -25,12 +24,14 @@ def init_board(size, mine):
     board[np.unravel_index(bombs, board.shape)] = 1
     return board
 
+
 # initalize minesweeper with a [size] x [size] board where all tiles are unopened
 
 
 def init_board_state(size):
     board_state = np.full((size, size), -1)
     return board_state
+
 
 # open a tile if not flagged. If bomb, mark lost condition.
 
@@ -40,11 +41,16 @@ def open_tile(board_state, board, row, col):
         board_state[row][col] = count_surrounding_bombs(board, row, col)
         if board_state[row][col] == 0:
             for r, c in coordinates:
-                if row+r >= 0 and col+c >= 0 and row+r < len(board) and col+c < len(board[0]):
-                    if board_state[row+r][col+c] == unopened:
-                        board_state = open_tile(
-                            board_state, board, row+r, col+c)
+                if (
+                    row + r >= 0
+                    and col + c >= 0
+                    and row + r < len(board)
+                    and col + c < len(board[0])
+                ):
+                    if board_state[row + r][col + c] == unopened:
+                        board_state = open_tile(board_state, board, row + r, col + c)
     return board_state
+
 
 # flag a single tile if it is unflagged and unopened. If flagged unflag
 
@@ -56,16 +62,23 @@ def flag_tile(board_state, row, col):
         board_state[row][col] = unopened
     return board_state
 
+
 # check how many bombs are surrounding a particular tile
 
 
 def count_surrounding_bombs(board, row, col):
     count = 0
     for r, c in coordinates:
-        if row+r >= 0 and col+c >= 0 and row+r < len(board) and col+c < len(board[0]):
-            if board[row+r][col+c] == mine:
+        if (
+            row + r >= 0
+            and col + c >= 0
+            and row + r < len(board)
+            and col + c < len(board[0])
+        ):
+            if board[row + r][col + c] == mine:
                 count += 1
     return count
+
 
 # check to see if game has been lost
 
@@ -77,6 +90,7 @@ def game_lost(board, board_state):
                 return True
     return False
 
+
 # check to see if game has been won
 
 
@@ -86,7 +100,7 @@ def game_won(board_state, bomb_count):
         for j in range(len(board_state[0])):
             if board_state[i][j] < 0:
                 open_tiles += 1
-    return open_tiles == len(board_state)*len(board_state[0]) - bomb_count
+    return open_tiles == len(board_state) * len(board_state[0]) - bomb_count
 
 
 # print a row of the game board to the console
@@ -95,21 +109,23 @@ def game_won(board_state, bomb_count):
 def print_row(board, board_state, row_num):
     for c in range(len(board[0])):
         if board_state[row_num][c] == unopened:
-            print('-', end=" ")
+            print("-", end=" ")
         elif board_state[row_num][c] == flaged:
-            print('f', end=" ")
+            print("f", end=" ")
         elif board[row_num][c] == mine:
-            print('m', end=" ")
+            print("m", end=" ")
         else:
             print(board_state[row_num][c], end=" ")
 
 
 # print the game board to the console
 
+
 def print_board(board, board_state):
     for r in range(len(board)):
         print_row(board, board_state, r)
         print()
+
 
 # game loop for minesweeper game mode
 
@@ -126,10 +142,19 @@ def printed_game_loop(mode):
         if mode == "human":
             read = input()
             read_split = read.split()
-            if len(read_split) == 3 and type(read_split[0]) == str and read_split[1].isdigit() and read_split[1].isdigit():
+            if (
+                len(read_split) == 3
+                and type(read_split[0]) == str
+                and read_split[1].isdigit()
+                and read_split[1].isdigit()
+            ):
                 opp = read_split[0]
                 r = int(read_split[1])
                 c = int(read_split[2])
+                if r < 0 or c < 0 or r >= board_size or c >= board_size:
+                    print("Out of bounds")
+                    continue
+
         else:
             opp, r, c = heuristic_model.ai_heuristic_logic(board_state)
         if opp == "open":
@@ -154,7 +179,7 @@ def trials(count):
             win += 1
     print("Trials: " + count)
     print("Wins: " + win)
-    print("Sucess Rate: " + win/count)
+    print("Sucess Rate: " + win / count)
 
 
 printed_game_loop("human")
