bc2432217a4ce0c8c16ac57932d37465f67d7c04
dtvirtue <dtvirtue@icloud.com>
2023-03-28 22:37:56 -0400

    Comments

--- unit_tests.py
+++ unit_tests.py
@@ -7,34 +7,46 @@ import unittest
 class TestInitBoard(unittest.TestCase):
 
     def test_init_board_10(self):
-        init_board = minesweeper.init_board(10, 3, 4, 4)
-        unique, counts = np.unique(init_board, return_counts=True)
-        counter = dict(zip(unique, counts))
         for _ in range(100):
+            init_board = minesweeper.init_board(10, 3, 4, 4)
+            unique, counts = np.unique(init_board, return_counts=True)
+            counter = dict(zip(unique, counts))
             self.assertEqual(
                 counter[1], 3, "incorrect number of mines in initial board state")
             self.assertEqual(init_board[4, 4], 0, "start tile should be empty")
 
-    def test_init_board_state():
-        
+    def test_init_board_state(self):
+        init_board_state = minesweeper.init_board_state(5)
+        self.assertEqual(init_board_state.shape, (5, 5),
+                         "board state is initalized to inccorect size")
+        self.assertEqual(np.all(init_board_state), True,
+                         "when initalized board state has an open tile")
+
+    def test_open_tile(self):
         pass
 
-    def test_open_tile():
+    def test_flag_tile(self):
         pass
 
-    def test_flag_tile():
+    def test_count_surrounding_bombs(self):
         pass
 
-    def test_count_surrounding_bombs():
+    def test_game_lost(self):
         pass
 
-    def test_game_lost():
+    def game_game_won(self):
         pass
 
-    def game_game_won():
+    def test_to_matrix(self):
         pass
 
-    def test_ai_heuristic_logic():
+    def test_analyze_matrix(self):
+        pass
+
+    def test_ai_heuristic_logic(self):
+        pass
+
+    def test_trials(self):
         pass
 
 
