add1fa5fe76e52e94f82b31e16c1c124c459a980
Sean Wiesner <seanwiesner@MacBook-Air.local>
2023-03-28 21:38:06 -0400

    finished test_count_surrounding_bombs

--- unit_tests.py
+++ unit_tests.py
@@ -31,26 +31,46 @@ class TestInitBoard(unittest.TestCase):
                 counter[1], 3, "incorrect number of mines in initial board state")
             self.assertEqual(init_board[4, 4], 0, "start tile should be empty")
 
-    def test_init_board_state():
+    def test_init_board_state(self):
 
         pass
 
-    def test_open_tile():
+    def test_open_tile(self):
+        # for size in range(10, 100):
+        #     f_indices = [(size / 2, (size / 2) + 1)] #1 arbitrary flag location
+        #     o_indices = [((size / 2) - 1, (size / 2) - 1), ((size / 2) - 2, (size / 2) - 2)] #2 arbitrary opened tiles
+        #     board_state = init_test_board_state(size, f_indices, o_indices)
+        #     f_index = f_indices[0]
+        #     o_index1 = o_indices[0]
+        #     o_index2 = o_indices[1]
         pass
 
-    def test_flag_tile():
+    def test_flag_tile(self):
         pass
 
-    def test_count_surrounding_bombs():
+    def test_count_surrounding_bombs(self):
+        m_indices = [(0, 0), (6, 6), (4, 5), (5, 6)]
+        board = init_test_board(10, m_indices)
+        actual_val = minesweeper.count_surrounding_bombs(board, 5, 5)
+        expected_val = 3
+        self.assertEqual(actual_val, expected_val,
+                         f"expected {expected_val} but got {actual_val}")
+        actual_val2 = minesweeper.count_surrounding_bombs(board, 2, 2)
+        expected_val2 = 0
+        self.assertEqual(actual_val2, expected_val2,
+                         f"expected {expected_val2} but got {actual_val2}")
+        actual_val3 = minesweeper.count_surrounding_bombs(board, 1, 0)
+        expected_val3 = 1
+        self.assertEqual(actual_val3, expected_val3,
+                         f"expected {expected_val3} but got {actual_val3}")
+
+    def test_game_lost(self):
         pass
 
-    def test_game_lost():
+    def game_game_won(self):
         pass
 
-    def game_game_won():
-        pass
-
-    def test_ai_heuristic_logic():
+    def test_ai_heuristic_logic(self):
         pass
 
 
