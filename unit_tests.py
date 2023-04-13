ca5e187e61311dd31a85a650e30fa444c1aaad14
dtvirtue <dtvirtue@icloud.com>
2023-04-13 14:21:50 -0400

    Update unit_tests.py
    flagging tested

--- unit_tests.py
+++ unit_tests.py
@@ -46,18 +46,8 @@ class TestInitBoardState(unittest.TestCase):
             "when initalized board state has an open tile",
         )
 
-    def test_open_tile(self):
-        pass
-
-    def test_flag_tile(self):
-        pass
-
-    def test_count_surrounding_bombs(self):
-        pass
-
 
 class TestOpenTile(unittest.TestCase):
-
     def test_open_tile_flagged(self):
         m_indices = [(0, 0)]
         board = init_test_board(10, m_indices)
@@ -65,8 +55,10 @@ class TestOpenTile(unittest.TestCase):
         board_state = init_test_board_state(10, f_indices, [])
         actual_val = minesweeper.open_tile(board_state, board, 0, 0)
         expected_val = board_state
-        self.assertTrue((actual_val == expected_val).all(),
-                        f"expected {expected_val} but got {actual_val}")
+        self.assertTrue(
+            (actual_val == expected_val).all(),
+            f"expected {expected_val} but got {actual_val}",
+        )
 
     def test_open_tile_bomb(self):
         m_indices = [(5, 5)]
@@ -74,8 +66,10 @@ class TestOpenTile(unittest.TestCase):
         board_state = init_test_board_state(10, [], [])
         actual_val = minesweeper.open_tile(board_state, board, 5, 5)
         expected_val = board_state.copy()
-        self.assertTrue((actual_val == expected_val).all(),
-                        f"expected {expected_val} but got {actual_val}")
+        self.assertTrue(
+            (actual_val == expected_val).all(),
+            f"expected {expected_val} but got {actual_val}",
+        )
 
     def test_open_tile_bomb_nearby(self):
         m_indices = [(4, 4)]
@@ -84,21 +78,45 @@ class TestOpenTile(unittest.TestCase):
         actual_val = minesweeper.open_tile(board_state, board, 5, 5)
         expected_val = board_state.copy()
         expected_val[5][5] = 1
-        self.assertTrue((actual_val == expected_val).all(),
-                        f"expected {expected_val} but got {actual_val}")
+        self.assertTrue(
+            (actual_val == expected_val).all(),
+            f"expected {expected_val} but got {actual_val}",
+        )
 
     def test_open_tile_no_bombs(self):
         board = init_test_board(10, [])
         board_state = init_test_board_state(10, [], [])
         actual_val = minesweeper.open_tile(board_state, board, 5, 5)
         expected_val = np.zeros((10, 10))
-        self.assertTrue((actual_val == expected_val).all(),
-                        f"expected {expected_val} but got {actual_val}")
+        self.assertTrue(
+            (actual_val == expected_val).all(),
+            f"expected {expected_val} but got {actual_val}",
+        )
 
 
 class TestFlagTile(unittest.TestCase):
     def test_flag_tile(self):
-        pass
+        m_indices = [(4, 4)]
+        board = init_test_board(10, m_indices)
+        board_state = init_test_board_state(10, [], [])
+        actual_val = minesweeper.flag_tile(board_state, 4, 4)
+        expected_val = init_test_board_state(10, m_indices, [])
+        self.assertTrue(
+            (actual_val == expected_val).all(),
+            f"expected {expected_val} but got {actual_val}",
+        )
+
+    def test_unflag_tile(self):
+        m_indices = [(4, 4)]
+        board = init_test_board(10, m_indices)
+        board_state = init_test_board_state(10, [], [])
+        flag_state = minesweeper.flag_tile(board_state, 4, 4)
+        actual_val = minesweeper.flag_tile(board_state, 4, 4)
+        expected_val = board_state
+        self.assertTrue(
+            (actual_val == expected_val).all(),
+            f"expected {expected_val} but got {actual_val}",
+        )
 
 
 class TestCountSurroundingBombs(unittest.TestCase):
