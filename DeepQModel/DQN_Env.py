3a81bddc49da2261779e61a53cdc939a0f3e509d
sean-wiesner <90277009+sean-wiesner@users.noreply.github.com>
2023-05-17 18:55:52 -0400

    initialized fields in DQEnv

--- DeepQModel/DQN_Env.py
+++ DeepQModel/DQN_Env.py
@@ -1,4 +1,16 @@
 import random
 import numpy as np
 import pandas as pd
+import minesweeper
+
+class DQEnvironment(object):
+    def __init__(self, bomb_count, board_size):
+        self.board_size = board_size
+        self.bomb_count = bomb_count
+        first_r = random.randint(1, board_size - 1)
+        first_c = random.randint(1, board_size - 1)
+        self.board = minesweeper.init_board(self.board_size, self.bomb_count, first_r, first_c)
+        self.init_board_state = minesweeper.open_tile(minesweeper.init_board_state(board_size), self.board, first_r, first_c)
+        #TODO Initialize other fields like rewards. There are probably others that I'm not aware of right now
+
 
