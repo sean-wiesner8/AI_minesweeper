537102eed19427c42abdf09611770f8f5972301d
Sean Wiesner <seanwiesner@dhcp-vl2042-23032.redrover.cornell.edu>
2023-05-17 22:44:07 -0400

    created DQEnvironment

--- DeepQModel/network.py
+++ DeepQModel/network.py
@@ -3,11 +3,11 @@ import torch.nn.functional as F
 
 class DQN(nn.Module):
     
-    def __init__(self, num_tiles, num_actions, num_dense):
+    def __init__(self, num_tiles, num_dense):
         super(DQN, self).__init__()
         self.layer1 = nn.Linear(num_tiles, num_dense)
         self.layer2 = nn.Linear(num_dense, num_dense)
-        self.layer3 = nn.Linear(num_dense, num_actions)
+        self.layer3 = nn.Linear(num_dense, num_tiles)
 
     def forward(self, x):
         x = F.relu(self.layer1(x))
